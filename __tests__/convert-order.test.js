a2748d8b5fb2679f321e1d0f3a73cbfc34b10347
Shandon Bowden <52051023+shandonb@users.noreply.github.com>
2023-12-08 10:52:10 -0500

    Added mocked responses to unit testing for X12 conversion

--- __tests__/convert-order.test.js
+++ __tests__/convert-order.test.js
@@ -1,13 +1,27 @@
+// Replacing environment variable with static value
+beforeAll(() => {
+    process.env.STORE_ISA = "testISA";
+});
+
+// Mock receiver ISA ID
+jest.mock('../app_modules/get-isa.js', () => jest.fn(() => 'testReceiver'));
+
+// Mocked barcode response from Shopify API call
+jest.mock('axios', () => ({
+    get: jest.fn(() => Promise.resolve({
+        data: {
+            variant: {
+                barcode: '012345678901'
+            }
+        }
+    }))
+}));
+
 const testOrder = require('./testOrder.json');
 const referenceOutput = require('./referenceOutput.json');
 
-beforeAll(() => {
-    process.env.STORE_ISA = "INSPIREME";
-
-});
-
 test('Test conversion to X12-like JSON', async () => {
-    const convertToX12 = require('../app_modules/convert-order.js');
-    const results = await convertToX12(testOrder, 'Orderful');
+    const convertToX12 = require('../app_modules/convert-order.js'); // Requiring the module inside the test to ensure environment variables set correctly
+    const results = await convertToX12(testOrder, 'testReceiver');
     expect(results).toEqual(referenceOutput);
 });
