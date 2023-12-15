0ae123f8aa4aeca7efda870934fb831f1d9c86d1
Shandon Bowden <52051023+shandonb@users.noreply.github.com>
2023-12-15 15:06:46 -0500

    Removed test values

--- __tests__/postOrder.test.js
+++ __tests__/postOrder.test.js
@@ -1,14 +1,14 @@
 const testOrder = require('./testOrder.json');
 
 beforeAll(() => {
-  process.env.STORE_ISA = "INSPIREME";
-  process.env.ORDERFUL_KEY = 'exampleAPIkey'
+  process.env.STORE_ISA = "EXAMPLE";
+  process.env.ORDERFUL_KEY = 'example'
 });
 
 test('Test posting an order to Orderful', async () => {
     const postToOrderful = require('../app_modules/post-order.js');
   try {
-    await postToOrderful(testOrder, "ExampleVendor");
+    await postToOrderful(testOrder, "Example");
     // You can log the result or check for successful execution
     console.log('Order posted to Orderful successfully');
   } catch (error) {
